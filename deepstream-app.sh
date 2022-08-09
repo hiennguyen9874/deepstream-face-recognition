451bb2f51557944e12d31a5c3fb21b0076beb3de
Xuan Hien <hiennguyen9874@gmail.com>
2022-08-09 17:25:35 +0000

    Fix Tensor metadata không tự thêm vô user metadata

--- deepstream-app.sh
+++ deepstream-app.sh
@@ -5,10 +5,9 @@ export GST_DEBUG_DUMP_DOT_DIR="logs/"
 # export NVDS_ENABLE_LATENCY_MEASUREMENT=1
 # export NVDS_ENABLE_COMPONENT_LATENCY_MEASUREMENT=1
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/deepstream/deepstream/lib:/opt/nvidia/deepstream/deepstream/lib/gst-plugins
 sudo bash sources/default.sh
 sudo bash sources/install.sh
 rm -rf logs/*
-/opt/nvidia/deepstream/deepstream-6.0/bin/deepstream-app -c samples/configs/deepstream_app.txt
+./bin/deepstream-app -c samples/configs/deepstream_app.txt
 python3 export_svg.py
 sudo bash sources/default.sh
